06f0026db36cbb2f50d7641b4cd1a49b29ad71fe
Sergio Ramos <sergio.ramos@unemat.br>
2021-09-08 08:53:37 -0400

    Conceitos de herança

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Avançando com Orientação a Objetos para me encontrar na sintaxe Python
 Nesse repositório dei uma revisada de como o python trabalha com os seguintes conceitos de Orientação a Objetos em Python: 
   - Métodos acessores
   - @property
+  - Herança
+  - Generalização/Especialização
+  - Método super()
--- modelo.py
+++ modelo.py
@@ -1,50 +1,42 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
 
-class Filme:
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
+        self.__likes += 1
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
-
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
